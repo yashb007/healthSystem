4e2022df4940fcf90ca4bd85b3d685b217034e93
anm0l555 <anmoltiwari555@gmail.com>
2020-09-01 02:36:31 +0530

    Second thing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
-
+import  Landing  from "./components/landingpage/Landing";
+import Header from "./components/layouts/Header";
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Landing />
       
     </div>
   );
---
+++ frontend/src/components/landingpage/Landing.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+function Landing() {
+    return(
+        <Fragment >
+            <h1 >Hi! from Anmol</h1>
+        </Fragment>
+    );
+}
+export  default Landing;

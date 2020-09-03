5b0465ed691cddf899b5d83f1fa6e1290aef21c8
Shubham Trivedi <strivedi_be19@thapar.edu>
2020-09-03 23:06:36 +0530

    adding langing page

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 
 const hello = makeStyles((theme) => ({
   root: {
@@ -26,7 +26,7 @@ function Header()  {
             <AppBar position="static">
                 <Toolbar>
                 <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
+                    {/* <MenuIcon /> */}
                 </IconButton>
                 <Typography variant="h6" className={classes.title}>
                     News

4799aad06529c1654e41e52822e3e81e7cbd6869
Shubham Trivedi <strivedi_be19@thapar.edu>
2020-09-04 00:05:39 +0530

    adding github link

--- frontend/src/components/landingpage/Landing.js
+++
@@ -1,9 +0,0 @@
-import React, { Fragment } from 'react';
-function Landing() {
-    return(
-        <Fragment >
-            <h1 >Hi! from Anmol</h1>
-        </Fragment>
-    );
-}
-export  default Landing;

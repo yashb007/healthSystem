3865d0495d1dcb9348b5d81579a062a7c16b0537
anm0l555 <anmoltiwari555@gmail.com>
2020-09-06 05:02:22 +0530

    Add User ,prescription and add doctor page updates along with some twitches

--- frontend/src/Hospital/dashboard/Dashboard.js
+++ frontend/src/Hospital/dashboard/Dashboard.js
@@ -7,6 +7,7 @@ import Card3 from './card3';
 import Card4 from './card4';
 import Card5 from './card5';
 import Card6 from './card6';
+
 function Hdashboard() {
     
     return(
--- frontend/src/Hospital/doctorlist/doctorlist.js
+++ frontend/src/Hospital/doctorlist/doctorlist.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js'; 
 import 'materialize-css/dist/css/materialize.min.css';
 import './doctorlist.css';
-import image1 from './img/Mask Group 3.png';
+import image1 from './img/croods.png';
 import Header2 from './layouts/header2';
 import Doctorcard from './doctor cards/doctorcard';
 class Doctorlist extends Component {
@@ -14,36 +14,36 @@ class Doctorlist extends Component {
         <div>
             <Header2 />
             <div className="row d_bkg"  >
-            
-                <div className="col s9 offset-s1 black-text " id="d_r1">
+                <div className="col s12 l5" id="dlist_col1">
+                    <img src={image1}  id="d_img" alt="" />
+                </div>
+                <div className="col s12 l7  black-text " >    
+                    <div className="input-field col s12" id="d_srch">
+                        
+                        <i className="material-icons prefix blgs_srch_icon black-text">search</i>
+                        <input type="text" placeholder="Search doctor here" id="autocomplete-input" className="autocomplete black-text" />
+                    
+                    </div>
                     <div className="row">
-                        <div className="col ">
-                            <img src={image1} className="avatar" id="d_img" alt="" />
-                        </div>
-                        <div className="input-field col auto" id="d_srch">
-                            
-                            <i className="material-icons prefix blgs_srch_icon black-text">search</i>
-                            <input type="text" placeholder="Search doctor here" id="autocomplete-input" className="autocomplete black-text" />
                         
-                        </div>
+                            <div className="col s12 m6 l4">
+                                <Doctorcard />
+                            </div>
+                            <div className="col s12 m6 l4">
+                                <Doctorcard />
+                            </div>
+                            <div className="col s12 m6 l4">
+                                <Doctorcard />
+                            </div>
+                            <div className="col s12 m6 l4">
+                                <Doctorcard />
+                            </div>
                         
                     </div>
                 </div>
             
             </div>
-            <div className="row">
-                <div className="col s10 offset-s1">
-                    <div className="col s12 m6 l3">
-                        <Doctorcard />
-                    </div>
-                    <div className="col s12 m6 l3">
-                        <Doctorcard />
-                    </div>
-                    <div className="col s12 m6 l3">
-                        <Doctorcard />
-                    </div>
-                </div>
-            </div>
+            
 
         </div>
          
